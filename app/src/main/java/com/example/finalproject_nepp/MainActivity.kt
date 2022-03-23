e4a73f55025eb4d7dc1cce8f03794a8fe5fe63e1
smin9408 <stmdaks888@naver.com>
2022-03-23 10:46:04 +0900

    #2 binding변수 생성 후 할당

--- app/src/main/java/com/example/finalproject_nepp/MainActivity.kt
+++ app/src/main/java/com/example/finalproject_nepp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.finalproject_nepp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject_nepp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
     }
 }

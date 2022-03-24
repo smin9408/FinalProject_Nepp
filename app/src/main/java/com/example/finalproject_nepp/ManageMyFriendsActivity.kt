b4311d85c32f2f76b74d4481734f9cae8b92a1f7
smin9408 <stmdaks888@naver.com>
2022-03-25 05:41:26 +0900

    #9 내 친구 목록 관리 화면 추가

---
+++ app/src/main/java/com/example/finalproject_nepp/ManageMyFriendsActivity.kt
@@ -0,0 +1,24 @@
+package com.example.finalproject_nepp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject_nepp.databinding.ActivityManageMyFriendsBinding
+
+class ManageMyFriendsActivity : BaseActivity() {
+
+    lateinit var binding: ActivityManageMyFriendsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_my_friends)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+}

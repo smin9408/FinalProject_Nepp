cffd5cde1648f4873e136ae224e780adee3aa5ce
smin9408 <stmdaks888@naver.com>
2022-03-24 10:50:37 +0900

    #1 인터페이스에, 로그인 기능을 위한 함수 작성

--- app/src/main/java/com/example/finalproject_nepp/api/APIList.kt
+++ app/src/main/java/com/example/finalproject_nepp/api/APIList.kt
@@ -1,4 +1,20 @@
 package com.example.finalproject_nepp.api
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface APIList {
+
+//    BASE_URL에 해당하는 서버에서, 어떤 기능들을 사용할건지 명시.
+
+    @POST("/user")
+    @FormUrlEncoded
+    fun postRequestLogin(
+        @Field("email") email: String,
+        @Field("password") pw: String
+    ): Call<JSONObject>
+
 }

a27bcf3323924f8b44dfa03829eac92d8bffc70e
smin9408 <stmdaks888@naver.com>
2022-03-24 18:02:29 +0900

    #9 2개의 프래그먼트 클래스 추가

---
+++ app/src/main/java/com/example/finalproject_nepp/fragments/MyProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.example.finalproject_nepp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.finalproject_nepp.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}

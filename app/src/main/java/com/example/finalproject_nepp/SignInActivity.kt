5adb067c0662abb7f19596631577a321935e55fa
smin9408 <stmdaks888@naver.com>
2022-03-24 11:07:21 +0900

    #1 로그인 기능 함수 호출 작성 중

--- app/src/main/java/com/example/finalproject_nepp/SignInActivity.kt
+++ app/src/main/java/com/example/finalproject_nepp/SignInActivity.kt
@@ -6,6 +6,10 @@ import androidx.databinding.DataBindingUtil
 import com.example.finalproject_nepp.api.APIList
 import com.example.finalproject_nepp.api.ServerAPI
 import com.example.finalproject_nepp.databinding.ActivitySignInBinding
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignInActivity : BaseActivity() {
 
@@ -20,7 +24,25 @@ class SignInActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-        apiList.postRequestLogin("","")
+        binding.btnLogin.setOnClickListener {
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPassword = binding.edtPassword.text.toString()
+
+            apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object :Callback<JSONObject>{
+                override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
+
+
+                    if (response.isSuccessful){
+
+                    }
+                }
+
+                override fun onFailure(call: Call<JSONObject>, t: Throwable) {
+                }
+            })
+
+        }
     }
 
     override fun setValues() {

213da4c193b5f88169a69637eb5a8e80f489a9ea
smin9408 <stmdaks888@naver.com>
2022-03-26 05:46:13 +0900

    #11 소셜 로그인 유형에 따른 이미지 처리

--- app/src/main/java/com/example/finalproject_nepp/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/example/finalproject_nepp/adapters/MyFriendAdapter.kt
@@ -20,7 +20,7 @@ class MyFriendAdapter(
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         var tempRow = convertView
-        if(tempRow == null){
+        if (tempRow == null) {
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
         }
 
@@ -31,10 +31,41 @@ class MyFriendAdapter(
         val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
         val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
         val txtEmail = row.findViewById<TextView>(R.id.txtEmail)
+        val imgSocialLoginLogo = row.findViewById<ImageView>(R.id.imgSocialLoginLogo)
 
         Glide.with(mContext).load(data.profile_img).into(imgProfile)
         txtNickname.text = data.nick_name
-        txtEmail.text = data.email
+
+//        사용자의 provider - "default" : 이메일 표시, 로그인하 sns에 따라 로고 변경
+        when (data.provider) {
+            "defalut" -> {
+//                이메일 표사
+                txtEmail.text = data.email
+
+//                로고 이미지 숨김
+                imgSocialLoginLogo.visibility = View.GONE
+            }
+            "kakao" -> {
+                txtEmail.text = "카카오 로그인"
+                imgSocialLoginLogo.visibility = View.VISIBLE
+                Glide.with(mContext).load(R.drawable.kakao_logo).into(imgSocialLoginLogo)
+            }
+            "facebook" -> {
+                txtEmail.text = "페북 로그인"
+                imgSocialLoginLogo.visibility = View.VISIBLE
+                Glide.with(mContext).load(R.drawable.facebook_logo).into(imgSocialLoginLogo)
+            }
+            "naver" -> {
+                txtEmail.text = "네이버 로그인"
+                imgSocialLoginLogo.visibility = View.VISIBLE
+
+//               Glide는 웹의 이미지 뿐 아니라, 우리 프로젝트 내부의 이미지도 불러낼 수 있다.
+                Glide.with(mContext).load(R.drawable.naver_logo).into(imgSocialLoginLogo)
+            }
+            else -> {
+
+            }
+        }
 
         return row
     }

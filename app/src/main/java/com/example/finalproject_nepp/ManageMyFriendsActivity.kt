0671f5c69faa95177d0dfee7f5cada8755ef8124
smin9408 <stmdaks888@naver.com>
2022-03-28 02:56:35 +0900

    #12 친구 목록 화면 변경 시도  - 내 친구 목록 / 나에게 들어온 친구 요청 나눠서 표시  - TabLayout / ViewPager 활용

--- app/src/main/java/com/example/finalproject_nepp/ManageMyFriendsActivity.kt
+++ app/src/main/java/com/example/finalproject_nepp/ManageMyFriendsActivity.kt
@@ -16,10 +16,6 @@ class ManageMyFriendsActivity : BaseActivity() {
 
     lateinit var binding: ActivityManageMyFriendsBinding
 
-    val mFriendList = ArrayList<UserData>()
-
-    lateinit var mAdapter : MyFriendAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_manage_my_friends)
@@ -28,9 +24,6 @@ class ManageMyFriendsActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-    }
-
-    override fun setValues() {
 
         binding.btnAddFriend.setOnClickListener {
 
@@ -39,37 +32,10 @@ class ManageMyFriendsActivity : BaseActivity() {
 
         }
 
-        getMyFriendListFromServer()
-
-        mAdapter = MyFriendAdapter(mContext, R.layout.my_friend_list_item, mFriendList)
-        binding.myFriendsListView.adapter = mAdapter
     }
 
-    fun getMyFriendListFromServer(){
-
-        apiList.getRequestFriendList("my").enqueue(object : Callback<BasicResponse>{
-            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
-
-                if(response.isSuccessful){
-
-                    val br = response.body()!!
-//                    br.data.friends는 UserData 목록으로 이미 내려옴.
-//                    목록의 내용물을 통째로 => mFriendList 변수의 내용물로 담자.
-
-                    mFriendList.addAll(br.data.friends)
-
-//                    어댑터 새로 고침
-                    mAdapter.notifyDataSetChanged()
-
-
-                }
-            }
-
-            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-            }
-        })
+    override fun setValues() {
 
     }
-
 
 }

f6598941ae8e69ccca298c42fad2435b9049d7f9
smin9408 <stmdaks888@naver.com>
2022-03-30 05:14:25 +0900

    #14 정보창 띄우기

--- app/src/main/java/com/example/finalproject_nepp/ViewMapActivity.kt
+++ app/src/main/java/com/example/finalproject_nepp/ViewMapActivity.kt
@@ -7,6 +7,7 @@ import com.example.finalproject_nepp.databinding.ActivityViewMapBinding
 import com.example.finalproject_nepp.datas.AppointmentData
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.PathOverlay
 import com.odsay.odsayandroidsdk.API
@@ -124,6 +125,26 @@ class ViewMapActivity : BaseActivity() {
                         val path = PathOverlay()
                         path.coords = stationList
                         path.map = naverMap
+
+//                        파싱을 추가로 하면, 소요시간 / 비용 정보도 얻을 수 있다. => infoWindow에 결합.
+                        val infoObj = firstPathObj.getJSONObject("info")
+
+                        val minutes = infoObj.getInt("totalTime")
+                        val payment = infoObj.getInt("payment")
+
+                        val infoStr = "이동 시간 : ${minutes}분, 비용 : ${payment}원"
+
+//                        정보창 띄우기
+                        val infoWindow = InfoWindow()
+
+                        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext){
+                            override fun getText(p0: InfoWindow): CharSequence {
+                                return infoStr
+                            }
+                        }
+
+                        infoWindow.open(marker)
+
                     }
 
                     override fun onError(p0: Int, p1: String?, p2: API?) {

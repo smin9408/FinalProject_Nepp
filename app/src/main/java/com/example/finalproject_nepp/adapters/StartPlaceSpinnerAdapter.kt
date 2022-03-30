efc2e3c25808e6a7e26cbf90c0bb1a0c72dc2132
smin9408 <stmdaks888@naver.com>
2022-03-30 09:54:48 +0900

    #16 getDropdownView 함수 추가 오버라이딩

--- app/src/main/java/com/example/finalproject_nepp/adapters/StartPlaceSpinnerAdapter.kt
+++ app/src/main/java/com/example/finalproject_nepp/adapters/StartPlaceSpinnerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.example.finalproject_nepp.R
 import com.example.finalproject_nepp.datas.PlaceData
 
@@ -25,9 +26,31 @@ class StartPlaceSpinnerAdapter(
 
         val data = mList[position]
 
+        val txtStartPlaceName = row.findViewById<TextView>(R.id.txtStartPlaceName)
 
+        txtStartPlaceName.text = data.name
 
         return row
+    }
+
+//    스피너가 선택 가능한 항목의 모양을 결정하는 함수.
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.start_place_spinner_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+        val txtStartPlaceName = row.findViewById<TextView>(R.id.txtStartPlaceName)
+
+        txtStartPlaceName.text = data.name
+
+        return row
+
     }
 
 }

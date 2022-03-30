df926d76089b1ddc142c558a547e37ce2d9d58c5
smin9408 <stmdaks888@naver.com>
2022-03-30 11:29:24 +0900

    #3 카톡 로그인 성공 > 우리 앱의 API서버에 전달 > 앱 서버의 토큰 저장

--- app/src/main/java/com/example/finalproject_nepp/api/APIList.kt
+++ app/src/main/java/com/example/finalproject_nepp/api/APIList.kt
@@ -84,5 +84,12 @@ interface APIList {
         @Field("is_primary") isPrimary: Boolean
     ): Call<BasicResponse>
 
+    @FormUrlEncoded
+    @POST("/user/social")
+    fun postRequestSocailLogin(
+        @Field("provider") provider: String,
+        @Field("uid") uid: String,
+        @Field("nick_name") nickname: String,
+    ) : Call<BasicResponse>
 
 }
